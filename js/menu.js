8b63dd23ab67517cfb646f72b528323b6fc23be6
Anna <ryavkina@mail.ru>
2021-11-06 12:53:02 +0500

    added partners.js and menu.js

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+const restaurant = 'tanuki'
+const renderItems = (data) => {
+    data.forEach( item => console.log(item))
+}
+
+fetch(`./db/${restaurant}.json`)
+    .then(response => response.json())
+    .then(data =>renderItems(data))
+    .catch(error => console.log(error))
